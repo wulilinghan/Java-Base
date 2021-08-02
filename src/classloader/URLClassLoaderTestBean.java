128b62f7810a9960b5c8350c737321ec1552169e
tlh <im.linghan@qq.com>
2021-08-02 22:31:16 +0800

    feat: 类加载器

---
+++ src/classloader/URLClassLoaderTestBean.java
@@ -0,0 +1,12 @@
+package classloader;
+
+/**
+ * @author ManJiis
+ * @since 2021-07-31
+ */
+public class URLClassLoaderTestBean {
+
+    public void sayHello() {
+        System.out.println("Hello from loaded Bean class for test!!!");
+    }
+}
